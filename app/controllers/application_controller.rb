efd1be6a307a50c42b54c9b8d4278f2e47d461e6
Alex Bujenita <dekc89@gmail.com>
2019-03-25 16:28:43 +0000

    linting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   private
    # Confirms a logged-in user.
-   def logged_in_user
+  def logged_in_user
     unless logged_in?
       flash[:danger] = "Please log in."
       redirect_to login_url
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,9 +6,7 @@ module SessionsHelper
 
   # Returns the current logged-in user (if any).
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
   # Returns true if the user is logged in, false otherwise.
@@ -16,10 +14,9 @@ module SessionsHelper
     !current_user.nil?
   end
 
-   # Logs out the current user.
-   def log_out
+  # Logs out the current user.
+  def log_out
     session.delete(:user_id)
     @current_user = nil
   end
-
 end
