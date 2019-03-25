eeaad16c7dbc5a89fb81ae452236c357918475fe
Alex Bujenita <dekc89@gmail.com>
2019-03-25 16:26:05 +0000

    relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :messages
+  has_many :groups, through: :messages
+
+
   validates :name, presence: true
   validates :nickname, presence: true, uniqueness: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_25_111024) do
+ActiveRecord::Schema.define(version: 2019_03_25_161500) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "owner_id"
   end
 
   create_table "members", force: :cascade do |t|
