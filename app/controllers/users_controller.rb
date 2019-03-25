e2e4466f06f139538e26635d70bd09bdf911c41b
Alex Bujenita <dekc89@gmail.com>
2019-03-25 15:42:59 +0000

    starting on authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,35 @@
 class UsersController < ApplicationController
 
-  before_action :get_user, only: [:show]
+  before_action :get_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:edit, :update]
+
 
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new(user_params())
+    @user = User.new(user_params(:name, :nickname, :age, :email, :password, :password_confirmation))
     if @user.save
       log_in @user
       redirect_to user_path(@user)
     else
       flash[:errors] = @user.errors.full_messages
       redirect_to new_user_path
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @user.update(user_params(:name, :email, :password, :password_confirmation))
+      flash[:success] = "Profile updated"
+      redirect_to user_path(current_user)
+    else
+      flash[:errors] = @user.errors.full_messages
+      redirect_to edit_user_path(current_user)
     end
   end
 
@@ -27,7 +43,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def user_params
-    params.require(:user).permit(:name, :nickname, :age, :email, :password, :password_confirmation)
+  def user_params(*args)
+    params.require(:user).permit(*args)
   end
+
+ 
 end
