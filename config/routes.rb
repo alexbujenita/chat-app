1e5e2d2edc5009ad88e1f19b222f1e1c899811be
Alex Bujenita <dekc89@gmail.com>
2019-03-25 11:16:37 +0000

    create models and homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  resources :groups
+  resources :members
+  resources :messages
+  root 'home#index'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
