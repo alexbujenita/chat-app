4f92f2231aa21c4f8bd3c51ac3083d92801fb7b6
Alex Bujenita <dekc89@gmail.com>
2019-03-25 12:58:30 +0000

    user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,32 @@
 class UsersController < ApplicationController
 
+  before_action :get_user, only: [:show]
+
   def new
     @user = User.new
   end
 
   def create
-    byebug
+    @user = User.new(user_params())
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      flash[:errors] = @user.errors.full_messages
+      redirect_to new_user_path
+    end
+  end
+
+  def show
+    
   end
 
   private
 
+  def get_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
-    
+    params.require(:user).permit(:name, :nickname, :age, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :name, presence: true
+  validates :nickname, presence: true, uniqueness: true
+
 end
