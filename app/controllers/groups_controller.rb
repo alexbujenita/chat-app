2319c7313fb4c4737a87cc54c288e444e87f4fdd
Alex Bujenita <dekc89@gmail.com>
2019-03-26 13:54:53 +0000

    creating groups and members

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,26 @@
 class GroupsController < ApplicationController
-
   before_action :correct_user, :users, only: [:new]
 
   def new
     @group = Group.new
   end
 
+  def create
+    @group = Group.create(group_params)
+
+    redirect_to user_path(current_user)
+    
+  end
+  
   private
+  
+  def group_params
+    owner_id = current_user.id
+    params[:group][:owner_id] = current_user.id.to_s
+    params[:group][:user_ids] = params[:group][:user_ids].push(owner_id.to_s)
+    params.require(:group).permit(:name,:owner_id, :user_ids => [])
+  end
+
   def users
     @users = User.all - [current_user]
   end
